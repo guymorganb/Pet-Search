7a19c8330fc7d14f1ddd84dff7d61ff537fc8d99
guymorganb <guymorganb@gmail.com>
2023-05-18 22:02:19 -0500

    made minor changes

--- assets/testScript.js
+++ assets/testScript.js
@@ -1,7 +1,4 @@
-// function toggleImageHighlight() {
-//     const checkbox = document.getElementById("category");
-//     checkbox.checked = !checkbox.checked;
-//   }
+
   const image = document.getElementById("img");
 
   image.addEventListener("click", () => {
@@ -16,37 +13,50 @@
 // let optionsData = {
 //     headers:{
 //         Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFb2hoMkRUOXRmdlZKSGhxY0lMTXV0Q3A2NWRqT1RpbjNqZ1A5eU96bkZTVE5SNU5yOCIsImp0aSI6IjIwNWVjMzk5ZmQ3NzNjMzllNjU4MjQ1NWJjMzE2MzQ1MDI1ZWQzYWYyM2MzZThhYWE4MzZmNWM4NTIyMzUxZGQzN2MwOWM4MjAxZWY5M2I4IiwiaWF0IjoxNjg0MzgxMDk3LCJuYmYiOjE2ODQzODEwOTcsImV4cCI6MTY4NDM4NDY5Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.WxTxSoDWe0oGrIUA4zvrNWARjFuKr3XdR-IHkRSp6jLVRoZ9ClM2VzrN9O7myiO1jzLXKrMmhMw45FJUNuIBIxegtloOWOPAROVmUum9yj7XwzuwhtjIhkMbDX16VDDStmqVPOg4tpc3VnFh8pwWLERrvjepwloOeKqLYHnM9gPvpEV1ai0stGrJL30jGMJiCgfyGYSBD7SGBr5S8ZMmgEZHX3Aop3QRRIaaSfGIspvUZIhCcMDH2Yo9Lgab_RhanzrlmLGXMuFN5aT779mz_noFkdA_5cH9BMh36Nowr4bDde0HSd2-GwnoAyhfZlnv9jY0WBSHf17llLnU4xMChw'
-//       }
-// }
-
-
-
-// async function getApi(url, options) {
-
-//     try{
-//         let response = await fetch(url,options)
-//         if(response.ok){
-//             let jsonData = await response.json();
-//             return jsonData;
-//         }else{
-//             throw new Error('network response not ok')
 //         }
-//     }catch(error){
-//         console.log('Error: ', error)
-//     }
-// //         .then(function (response) {
-// //       if (response.status == 200) {
-// //         console.log(response.status)
-// //       }
-// //       return response.json();
-// //   }).then(function(data){
-// //     console.log(data);
-// //   })
 // }
+
+const url = 'https://dog-breeds2.p.rapidapi.com/dog_breeds';;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d00143dfemshcfa11c25093ca9bp1442f9jsnf5e008afc921',
+		'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
+	}
+};
+
+
+
+async function getApi(url, options) {
+
+    try{
+        let response = await fetch(url, options);
+        if(response.ok){
+            let data = await response.json();
+            console.log(data)
+            return data;
+        }else{
+            throw new Error('network response not ok')
+        }
+    }catch(error){
+        console.log('Error: ', error)
+    }
+
+}
+getApi(url, options)
 
 // function getPetData(){getApi().then(function(){
 
-// })}
+
+// .then(function (response) {
+//     if (response.status == 200) {
+//       console.log(response.status)
+//     }
+//     return response.json();
+// }).then(function(data){
+//   console.log(data);
+// })
+
 
 // getPetsApi();
 
@@ -81,3 +91,4 @@
 //   }
   
 //   personalityApi();
+
