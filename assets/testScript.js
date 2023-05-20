c81c68fb83ff0e7ebb50a22528c5ce680ccea698
guymorganb <guymorganb@gmail.com>
2023-05-20 11:32:51 -0500

    added Api calls

--- assets/testScript.js
+++ assets/testScript.js
@@ -1,12 +1,35 @@
 
-  const image = document.getElementById("img");
 
-  image.addEventListener("click", () => {
-    console.log("Image is clicked.");
-    // You can also check the checkbox state here if needed
-    const checkbox = document.getElementById("category");
-    console.log("Checkbox is checked:", checkbox.checked);
-  });
+
+// curl -d "grant_type=client_credentials&client_id=Eohh2DT9tfvVJHhqcILMutCp65djOTin3jgP9yOznFSTNR5Nr8&client_secret=77mOBQIaScP5uUxJw66mkNdWTBJ5wwQvrz5QXgdK" https://api.petfinder.com/v2/oauth2/token
+
+// "Authorization: Bearer {YOUR_ACCESS_TOKEN}" GET https://api.petfinder.com/v2/animals?type=dog&page=2
+
+
+
+
+let requestUrl = 'Authorization: GET https://api.petfinder.com/v2/animals?type=dog&page=2'
+
+function getApi() {
+
+  fetch('https://api.petfinder.com/v2/animals?type=dog&page=2',{
+    headers:{
+      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFb2hoMkRUOXRmdlZKSGhxY0lMTXV0Q3A2NWRqT1RpbjNqZ1A5eU96bkZTVE5SNU5yOCIsImp0aSI6IjIwNWVjMzk5ZmQ3NzNjMzllNjU4MjQ1NWJjMzE2MzQ1MDI1ZWQzYWYyM2MzZThhYWE4MzZmNWM4NTIyMzUxZGQzN2MwOWM4MjAxZWY5M2I4IiwiaWF0IjoxNjg0MzgxMDk3LCJuYmYiOjE2ODQzODEwOTcsImV4cCI6MTY4NDM4NDY5Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.WxTxSoDWe0oGrIUA4zvrNWARjFuKr3XdR-IHkRSp6jLVRoZ9ClM2VzrN9O7myiO1jzLXKrMmhMw45FJUNuIBIxegtloOWOPAROVmUum9yj7XwzuwhtjIhkMbDX16VDDStmqVPOg4tpc3VnFh8pwWLERrvjepwloOeKqLYHnM9gPvpEV1ai0stGrJL30jGMJiCgfyGYSBD7SGBr5S8ZMmgEZHX3Aop3QRRIaaSfGIspvUZIhCcMDH2Yo9Lgab_RhanzrlmLGXMuFN5aT779mz_noFkdA_5cH9BMh36Nowr4bDde0HSd2-GwnoAyhfZlnv9jY0WBSHf17llLnU4xMChw'
+    }
+  })
+    .then(function (response) {
+      if (response.status == 200) {
+        console.log(response.status)
+      }
+      return response.json();
+  }).then(function(data){
+    console.log(data);
+  })
+
+}
+
+getApi();
+
 
 
 // let petsUrl = 'https://api.petfinder.com/v2/animals?type=dog&page=2'
@@ -15,80 +38,33 @@
 //         Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFb2hoMkRUOXRmdlZKSGhxY0lMTXV0Q3A2NWRqT1RpbjNqZ1A5eU96bkZTVE5SNU5yOCIsImp0aSI6IjIwNWVjMzk5ZmQ3NzNjMzllNjU4MjQ1NWJjMzE2MzQ1MDI1ZWQzYWYyM2MzZThhYWE4MzZmNWM4NTIyMzUxZGQzN2MwOWM4MjAxZWY5M2I4IiwiaWF0IjoxNjg0MzgxMDk3LCJuYmYiOjE2ODQzODEwOTcsImV4cCI6MTY4NDM4NDY5Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.WxTxSoDWe0oGrIUA4zvrNWARjFuKr3XdR-IHkRSp6jLVRoZ9ClM2VzrN9O7myiO1jzLXKrMmhMw45FJUNuIBIxegtloOWOPAROVmUum9yj7XwzuwhtjIhkMbDX16VDDStmqVPOg4tpc3VnFh8pwWLERrvjepwloOeKqLYHnM9gPvpEV1ai0stGrJL30jGMJiCgfyGYSBD7SGBr5S8ZMmgEZHX3Aop3QRRIaaSfGIspvUZIhCcMDH2Yo9Lgab_RhanzrlmLGXMuFN5aT779mz_noFkdA_5cH9BMh36Nowr4bDde0HSd2-GwnoAyhfZlnv9jY0WBSHf17llLnU4xMChw'
 //         }
 // }
-
-const url = 'https://dog-breeds2.p.rapidapi.com/dog_breeds';;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '0d00143dfemshcfa11c25093ca9bp1442f9jsnf5e008afc921',
-		'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
-	}
-};
-
-
-
-async function getApi(url, options) {
-
-    try{
-        let response = await fetch(url, options);
-        if(response.ok){
-            let data = await response.json();
-            console.log(data)
-            return data;
-        }else{
-            throw new Error('network response not ok')
-        }
-    }catch(error){
-        console.log('Error: ', error)
-    }
-
-}
-getApi(url, options)
-
-// function getPetData(){getApi().then(function(){
+/////////////////////////////gets wikapedia api for dog breeds////////////////////////////////////////
+// const url = 'https://dog-breeds2.p.rapidapi.com/dog_breeds';;
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '0d00143dfemshcfa11c25093ca9bp1442f9jsnf5e008afc921',
+// 		'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
+// 	}
+// };
 
 
-// .then(function (response) {
-//     if (response.status == 200) {
-//       console.log(response.status)
-//     }
-//     return response.json();
-// }).then(function(data){
-//   console.log(data);
-// })
 
+// async function getApi(url, options) {
 
-// getPetsApi();
-
-// let personalityUrl = 'https://big-five-personality-insights.p.rapidapi.com/api/big5'
-
-// function personalityApi() {
-
-//     fetch(personalityUrl,{
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/json',
-//             'X-RapidAPI-Key': '0d00143dfemshcfa11c25093ca9bp1442f9jsnf5e008afc921',
-//             'X-RapidAPI-Host': 'big-five-personality-insights.p.rapidapi.com'
-//         },
-//         body: [
-//             {
-//                 id: '1',
-//                 language: 'en',
-//                 text: 'I love this service'
-//             }
-//         ]
-//     })
-//       .then(function (response) {
-//         if (response.status == 200) {
-//           console.log(response.status)
+//     try{
+//         let response = await fetch(url, options);
+//         if(response.ok){
+//             let data = await response.json();
+//             console.log(data)
+//             return data;
+//         }else{
+//             throw new Error('network response not ok')
 //         }
-//         return response.json();
-//     }).then(function(data){
-//       console.log(data);
-//     })
-  
-//   }
-  
-//   personalityApi();
+//     }catch(error){
+//         console.log('Error: ', error)
+//     }
 
+// }
+// getApi(url, options)
+/////////////////////////////gets wikapedia api for dog breeds////////////////////////////////////////
